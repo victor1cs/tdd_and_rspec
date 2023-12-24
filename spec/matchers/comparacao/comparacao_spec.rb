43d1a7da262c6361a6b764b309c87e4b769e5bbf
Victor Correa <victor1cs@outlook.com>
2023-12-23 23:42:57 -0300

    comparation matchers

---
+++ spec/matchers/comparacao/comparacao_spec.rb
@@ -0,0 +1,29 @@
+describe 'Matchers de comparacao' do
+  it '#equal - Testa se eh o mesmo objeto' do
+    x = "ruby"
+    y = "ruby"
+    expect(x).to equal(x)
+    expect(x).not_to equal(y)
+  end
+
+  it '#be - Testa se eh o mesmo objeto' do
+    x = "ruby"
+    y = "ruby"
+    expect(x).to be(x)
+    expect(x).not_to be(y)
+  end
+
+  it '#eql - Testa o valor' do
+    x = "ruby"
+    y = "ruby"
+    expect(x).to eql(x)
+    expect(x).to eql(y)
+  end
+
+  it '#eq - Testa o valor' do
+    x = "ruby"
+    y = "ruby"
+    expect(x).to eq(x)
+    expect(x).to eq(y)
+  end
+end
