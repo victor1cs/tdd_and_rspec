91759f1955ae6e7490602504ba071958630aa0a9
Victor Correa <victor1cs@outlook.com>
2023-12-23 23:15:44 -0300

    refactoring the first tests using subject

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,21 +1,21 @@
 require 'calculator'
 
 describe Calculator do
+
+  subject(:calc) { Calculator.new() }
+
   context '#sum' do
     it 'with positive numbers' do
-      calc   = Calculator.new
       result = calc.sum(5, 7)
       expect(result).to eq(12)
     end
 
     it 'with negative and positive numbers' do
-      calc   = Calculator.new
       result = calc.sum(-5, 7)
       expect(result).to eq(2)
     end
 
     it 'with negative numbers' do
-      calc   = Calculator.new
       result = calc.sum(-5, -7)
       expect(result).to eq(-12)
     end
