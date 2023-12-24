354c062eb93d44238c9239440d74f34f7a3d4639
Victor Correa <victor1cs@outlook.com>
2023-12-23 23:08:41 -0300

    refactoring the first tests

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,15 +1,23 @@
 require 'calculator'
+
 describe Calculator do
-  it 'sum method for 2 numbers' do
-    calc   = Calculator.new
-    result = calc.sum(5, 7)
-    expect(result).to eq(12)
-  end
+  context '#sum' do
+    it 'with positive numbers' do
+      calc   = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
 
-  it 'sum method for 2 numbers with negative numbers' do
-    calc   = Calculator.new
-    result = calc.sum(-5, 7)
-    expect(result).to eq(2)
-  end
+    it 'with negative and positive numbers' do
+      calc   = Calculator.new
+      result = calc.sum(-5, 7)
+      expect(result).to eq(2)
+    end
 
+    it 'with negative numbers' do
+      calc   = Calculator.new
+      result = calc.sum(-5, -7)
+      expect(result).to eq(-12)
+    end
+  end
 end
