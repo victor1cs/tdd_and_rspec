f09c386aa5b0061872d266fc0709e4cf2d779368
Victor Correa <victor1cs@outlook.com>
2024-01-07 09:28:27 -0300

    Custom matchers

--- spec/matchers/satisfy/satisfy_spec.rb
+++ spec/matchers/satisfy/satisfy_spec.rb
@@ -5,5 +5,5 @@ describe 'satisfy' do
       x % 3 == 0 
     end
   }
-  it { expect(10).to satisfy('be a multiple of 3') { |x| x % 2 == 0 } }
+  it { expect(10).to satisfy('be a multiple of 2') { |x| x % 2 == 0 } }
 end
